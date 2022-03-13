3607c6b1e20c85c8f0bc10fae558f5e475385296
Zed <emailzed03@gmail.com>
2022-03-13 16:07:41 +1100

    added an export enum when the user clicks the export button

---
+++ Meditor/Utility/ExportEnum.swift
@@ -0,0 +1,20 @@
+//
+//  ExportEnum.swift
+//  Meditor
+//
+//  Created by Zed on 13/3/2022.
+//
+
+import Foundation
+
+
+enum ExportTo: Equatable {
+    case PDF
+    case HTML
+    case TEXT
+    case NONE
+    
+//    static func ==(lhs: ExportTo, rhs: ExportTo) -> Bool {
+//        return lhs.hashValue == rhs.hashValue
+//    }
+}
